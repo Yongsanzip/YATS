44c4fcdf40d0218ffd14ad59e2cd3932cf5d20b4
nine-hundred <ninehundred@outlook.kr>
2018-05-16 17:32:50 +0900

    Api test function is done

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,43 +1,130 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Library\Anyalazum;
 use Illuminate\Http\Request;
+use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\Facades\App;
-
+use Illuminate\Support\Facades\Input;
+use Illuminate\Validation\Rules\In;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class ApiController extends Controller
 {
     //
     public $PARAMS;
 
-    public function setParams() {
-
-    }
-
     public function send() {
-        $curl = curl_init();
-//        $url = "https://carderla.co.kr/index.php/Car/getCar";
-
         $an = new Anyalazum();
-//        $method = $an->httpMethod($api);
+        $url = Input::get('url');
+        $method = Input::get('method');
 
-        $url = $_POST['url'];
-        curl_setopt($curl, CURLOPT_URL, $url);
-        $reponse = curl_exec($curl);
-        echo $reponse;
-        /*
-        if ($method->name == 'GET') {
-            $url = $api->url.'?'.http_build_query($api->params, '','&');
-            curl_setopt($curl, CURLOPT_URL, $url);
-            $res = curl_exec();
-            var_dump($res);
-            curl_close();
-        } else {
+        $headers = json_decode(Input::get('headers'), true);
+        $header = array();
+        if (is_array($headers)) {
+            foreach ($headers as $key => $value) {
+                $header = $an->array_push_assoc($header, $key, $value->key.":".$value->data);
+            }
+        }
 
-        }*/
-        //return $t = array($statusCode, $response);
+        $body = json_decode(Input::get('datas'), true);
+        $fileDatas = array();
+        $datas = array();
+
+        if (is_array($body)) {
+            foreach ($body as $key => $value) {
+                if ($value['data_type'] === 'file') {
+                    $fileDatas = $an->array_push_assoc($fileDatas, $value['key'], $_FILES['video']['tmp_name']);
+                } else {
+                    $datas = $an->array_push_assoc($datas, $value['key'], $value['data']);
+                }
+            }
+        }
+        //$datas  = $an->array_push_assoc($datas,'video',$_FILES['video']);
+
+        $response = '';
+        $responseCode = '';
+        $curl = curl_init();
+        switch ($method) {
+            case 'GET' :
+                $datas = rawurldecode(http_build_query($datas));
+                $url = $url.'?'.$datas;
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array("content-type::multipart/form-data"));
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+                curl_setopt($curl, CURLOPT_HTTPGET, true);
+                $response = curl_exec($curl);
+                $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+                curl_close($curl);
+
+                break;
+            case 'POST' :
+                foreach ($fileDatas as $key => $value) {
+                    $datas = $an->array_push_assoc($datas, $key,
+                        new \CURLFile($_FILES[$key]['tmp_name'], $_FILES[$key]['type'], $_FILES[$key]['name'])
+                    );
+                }
+
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $datas);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+                curl_setopt($curl, CURLOPT_POST, true);
+                $response = curl_exec($curl);
+                echo $response;
+                curl_close($curl);
+                break;
+            case 'PUT' :
+                foreach ($fileDatas as $key => $value) {
+                    $datas = $an->array_push_assoc($datas, $key,
+                        new \CURLFile($_FILES[$key]['tmp_name'], $_FILES[$key]['type'], $_FILES[$key]['name'])
+                    );
+                }
+
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $datas);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+                $response = curl_exec($curl);
+                $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+                curl_close($curl);
+                break;
+            case  'DELETE' :
+                foreach ($fileDatas as $key => $value) {
+                    $datas = $an->array_push_assoc($datas, $key,
+                        new \CURLFile($_FILES[$key]['tmp_name'], $_FILES[$key]['type'], $_FILES[$key]['name'])
+                    );
+                }
+
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $datas);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+                $response = curl_exec($curl);
+                $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+                curl_close($curl);
+                break;
+            case 'PATCH' :
+                foreach ($fileDatas as $key => $value) {
+                    $datas = $an->array_push_assoc($datas, $key,
+                        new \CURLFile($_FILES[$key]['tmp_name'], $_FILES[$key]['type'], $_FILES[$key]['name'])
+                    );
+                }
+
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $datas);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PATCH");
+
+                $response = curl_exec($curl);
+                $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+                curl_close($curl);
+                break;
+
+            default :
+                return view('error');
+                break;
+        }
+//*/
     }
 
 }
